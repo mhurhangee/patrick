c43a9fdb8daa859d009dbf3a1c3b7ad616ebfadc
Michael Hurhangee <m.hurhangee@me.com>
2026-05-20 22:21:30 +0100

    refactor: slim toolbars to patent-relevant commands
    Fixed toolbar: undo/redo, AI, import/export, comment, mode only.
    Floating toolbar: full inline marks including subscript, superscript,
    highlight, inline equation, date, link, comment, suggestion.
    Restore font-color-toolbar-button (used by table cell backgrounds).

--- apps/frontend/src/components/ui/fixed-toolbar-buttons.tsx
+++ apps/frontend/src/components/ui/fixed-toolbar-buttons.tsx
@@ -1,45 +1,15 @@
 "use client"
 
-import {
-	ArrowUpToLineIcon,
-	BoldIcon,
-	Code2Icon,
-	HighlighterIcon,
-	ItalicIcon,
-	StrikethroughIcon,
-	UnderlineIcon,
-	WandSparklesIcon,
-} from "lucide-react"
-import { KEYS } from "platejs"
+import { ArrowUpToLineIcon, WandSparklesIcon } from "lucide-react"
 import { useEditorReadOnly } from "platejs/react"
-import * as React from "react"
 
 import { AIToolbarButton } from "./ai-toolbar-button"
-import { AlignToolbarButton } from "./align-toolbar-button"
 import { CommentToolbarButton } from "./comment-toolbar-button"
 import { ExportToolbarButton } from "./export-toolbar-button"
 import { RedoToolbarButton, UndoToolbarButton } from "./history-toolbar-button"
 import { ImportToolbarButton } from "./import-toolbar-button"
-import {
-	IndentToolbarButton,
-	OutdentToolbarButton,
-} from "./indent-toolbar-button"
-import { InsertToolbarButton } from "./insert-toolbar-button"
-import { LineHeightToolbarButton } from "./line-height-toolbar-button"
-import { LinkToolbarButton } from "./link-toolbar-button"
-import {
-	BulletedListToolbarButton,
-	NumberedListToolbarButton,
-	TodoListToolbarButton,
-} from "./list-toolbar-button"
-import { MarkToolbarButton } from "./mark-toolbar-button"
-import { MediaToolbarButton } from "./media-toolbar-button"
 import { ModeToolbarButton } from "./mode-toolbar-button"
-import { MoreToolbarButton } from "./more-toolbar-button"
-import { TableToolbarButton } from "./table-toolbar-button"
-import { ToggleToolbarButton } from "./toggle-toolbar-button"
 import { ToolbarGroup } from "./toolbar"
-import { TurnIntoToolbarButton } from "./turn-into-toolbar-button"
 
 export function FixedToolbarButtons() {
 	const readOnly = useEditorReadOnly()
@@ -63,70 +33,7 @@ export function FixedToolbarButtons() {
 						<ExportToolbarButton>
 							<ArrowUpToLineIcon />
 						</ExportToolbarButton>
-
 						<ImportToolbarButton />
-					</ToolbarGroup>
-
-					<ToolbarGroup>
-						<InsertToolbarButton />
-						<TurnIntoToolbarButton />
-					</ToolbarGroup>
-
-					<ToolbarGroup>
-						<MarkToolbarButton nodeType={KEYS.bold} tooltip="Bold (⌘+B)">
-							<BoldIcon />
-						</MarkToolbarButton>
-
-						<MarkToolbarButton nodeType={KEYS.italic} tooltip="Italic (⌘+I)">
-							<ItalicIcon />
-						</MarkToolbarButton>
-
-						<MarkToolbarButton
-							nodeType={KEYS.underline}
-							tooltip="Underline (⌘+U)"
-						>
-							<UnderlineIcon />
-						</MarkToolbarButton>
-
-						<MarkToolbarButton
-							nodeType={KEYS.strikethrough}
-							tooltip="Strikethrough (⌘+⇧+M)"
-						>
-							<StrikethroughIcon />
-						</MarkToolbarButton>
-
-						<MarkToolbarButton nodeType={KEYS.code} tooltip="Code (⌘+E)">
-							<Code2Icon />
-						</MarkToolbarButton>
-
-					</ToolbarGroup>
-
-					<ToolbarGroup>
-						<AlignToolbarButton />
-
-						<NumberedListToolbarButton />
-						<BulletedListToolbarButton />
-						<TodoListToolbarButton />
-						<ToggleToolbarButton />
-					</ToolbarGroup>
-
-					<ToolbarGroup>
-						<LinkToolbarButton />
-						<TableToolbarButton />
-					</ToolbarGroup>
-
-					<ToolbarGroup>
-						<MediaToolbarButton nodeType={KEYS.img} />
-					</ToolbarGroup>
-
-					<ToolbarGroup>
-						<LineHeightToolbarButton />
-						<OutdentToolbarButton />
-						<IndentToolbarButton />
-					</ToolbarGroup>
-
-					<ToolbarGroup>
-						<MoreToolbarButton />
 					</ToolbarGroup>
 				</>
 			)}
@@ -134,9 +41,6 @@ export function FixedToolbarButtons() {
 			<div className="grow" />
 
 			<ToolbarGroup>
-				<MarkToolbarButton nodeType={KEYS.highlight} tooltip="Highlight">
-					<HighlighterIcon />
-				</MarkToolbarButton>
 				<CommentToolbarButton />
 			</ToolbarGroup>
 
--- apps/frontend/src/components/ui/floating-toolbar-buttons.tsx
+++ apps/frontend/src/components/ui/floating-toolbar-buttons.tsx
@@ -1,29 +1,32 @@
 "use client"
 
+import { insertDate } from "@platejs/date"
 import {
 	BoldIcon,
+	CalendarIcon,
 	Code2Icon,
+	HighlighterIcon,
 	ItalicIcon,
 	StrikethroughIcon,
+	SubscriptIcon,
+	SuperscriptIcon,
 	UnderlineIcon,
 	WandSparklesIcon,
 } from "lucide-react"
 import { KEYS } from "platejs"
-import { useEditorReadOnly } from "platejs/react"
-import * as React from "react"
+import { useEditorReadOnly, useEditorRef } from "platejs/react"
 
 import { AIToolbarButton } from "./ai-toolbar-button"
 import { CommentToolbarButton } from "./comment-toolbar-button"
 import { InlineEquationToolbarButton } from "./equation-toolbar-button"
 import { LinkToolbarButton } from "./link-toolbar-button"
 import { MarkToolbarButton } from "./mark-toolbar-button"
-import { MoreToolbarButton } from "./more-toolbar-button"
 import { SuggestionToolbarButton } from "./suggestion-toolbar-button"
-import { ToolbarGroup } from "./toolbar"
-import { TurnIntoToolbarButton } from "./turn-into-toolbar-button"
+import { ToolbarButton, ToolbarGroup } from "./toolbar"
 
 export function FloatingToolbarButtons() {
 	const readOnly = useEditorReadOnly()
+	const editor = useEditorRef()
 
 	return (
 		<>
@@ -37,36 +40,37 @@ export function FloatingToolbarButtons() {
 					</ToolbarGroup>
 
 					<ToolbarGroup>
-						<TurnIntoToolbarButton />
-
 						<MarkToolbarButton nodeType={KEYS.bold} tooltip="Bold (⌘+B)">
 							<BoldIcon />
 						</MarkToolbarButton>
-
 						<MarkToolbarButton nodeType={KEYS.italic} tooltip="Italic (⌘+I)">
 							<ItalicIcon />
 						</MarkToolbarButton>
-
-						<MarkToolbarButton
-							nodeType={KEYS.underline}
-							tooltip="Underline (⌘+U)"
-						>
+						<MarkToolbarButton nodeType={KEYS.underline} tooltip="Underline (⌘+U)">
 							<UnderlineIcon />
 						</MarkToolbarButton>
-
-						<MarkToolbarButton
-							nodeType={KEYS.strikethrough}
-							tooltip="Strikethrough (⌘+⇧+M)"
-						>
+						<MarkToolbarButton nodeType={KEYS.strikethrough} tooltip="Strikethrough (⌘+⇧+M)">
 							<StrikethroughIcon />
 						</MarkToolbarButton>
-
+						<MarkToolbarButton nodeType={KEYS.subscript} tooltip="Subscript (⌘+,)">
+							<SubscriptIcon />
+						</MarkToolbarButton>
+						<MarkToolbarButton nodeType={KEYS.superscript} tooltip="Superscript (⌘+.)">
+							<SuperscriptIcon />
+						</MarkToolbarButton>
 						<MarkToolbarButton nodeType={KEYS.code} tooltip="Code (⌘+E)">
 							<Code2Icon />
 						</MarkToolbarButton>
-
+						<MarkToolbarButton nodeType={KEYS.highlight} tooltip="Highlight">
+							<HighlighterIcon />
+						</MarkToolbarButton>
 						<InlineEquationToolbarButton />
-
+						<ToolbarButton
+							tooltip="Insert Date"
+							onClick={() => insertDate(editor, { select: true })}
+						>
+							<CalendarIcon />
+						</ToolbarButton>
 						<LinkToolbarButton />
 					</ToolbarGroup>
 				</>
@@ -75,8 +79,6 @@ export function FloatingToolbarButtons() {
 			<ToolbarGroup>
 				<CommentToolbarButton />
 				<SuggestionToolbarButton />
-
-				{!readOnly && <MoreToolbarButton />}
 			</ToolbarGroup>
 		</>
 	)
